2db04fff06a96dc8a69fdca2ea0a7d46edef0397
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2024-06-15 00:28:25 -0300

    remove comments, save require call inside a local var

--- lua/bielsz/lazy/lsp.lua
+++ lua/bielsz/lazy/lsp.lua
@@ -22,23 +22,19 @@ return {
 					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
 
+				local tl_builtin = require("telescope.builtin")
 				--  To jump back, press <C-t>.
-				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+				map("gd", tl_builtin.lsp_definitions, "[G]oto [D]efinition")
+				map("gr", tl_builtin.lsp_references, "[G]oto [R]eferences")
+				map("gI", tl_builtin.lsp_implementations, "[G]oto [I]mplementation")
+				map("<leader>D", tl_builtin.lsp_type_definitions, "Type [D]efinition")
+				map("<leader>ds", tl_builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+				map("<leader>ws", tl_builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 				map("K", vim.lsp.buf.hover, "Hover Documentation")
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-				-- The following two autocommands are used to highlight references of the
-				-- word under your cursor when your cursor rests there for a little while.
-				--    See `:help CursorHold` for information about when this is executed
-				--
-				-- When you move your cursor, the highlights will be cleared (the second autocommand).
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
 				if client and client.server_capabilities.documentHighlightProvider then
 					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
@@ -63,10 +59,6 @@ return {
 					})
 				end
 
-				-- The following autocommand is used to enable inlay hints in your
-				-- code, if the language server you are using supports them
-				--
-				-- This may be unwanted, since they displace some of your code
 				if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
 					map("<leader>th", function()
 						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
