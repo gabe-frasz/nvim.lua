1d7bcd3a8110355d97a46bd4969b55902616b987
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2024-06-15 00:31:55 -0300

    remove map description, remove comments, add formatters for elixir and go

--- lua/bielsz/lazy/conform.lua
+++ lua/bielsz/lazy/conform.lua
@@ -8,15 +8,11 @@ return {
 				require("conform").format({ async = true, lsp_fallback = true })
 			end,
 			mode = "",
-			desc = "[F]ormat buffer",
 		},
 	},
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
-			-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- have a well standardized coding style. You can add additional
-			-- languages here or re-enable it for the disabled ones.
 			local disable_filetypes = { c = true, cpp = true }
 			return {
 				timeout_ms = 500,
@@ -27,6 +23,8 @@ return {
 			lua = { "stylua" },
 			javascript = { "prettier" },
 			typescript = { "prettier" },
+			elixir = { "mix" },
+			go = { "goimports", "gofmt" },
 		},
 	},
 }
