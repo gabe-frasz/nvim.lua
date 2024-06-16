a89b18357397fd987df515b5fac80908d0867978
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2024-06-16 16:58:04 -0300

    remove descriptions

--- lua/bielsz/lazy/telescope.lua
+++ lua/bielsz/lazy/telescope.lua
@@ -6,13 +6,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
-
-			-- `build` is used to run some command when the plugin is installed/updated.
-			-- This is only run then, not every time Neovim starts up.
 			build = "make",
-
-			-- `cond` is a condition used to determine whether this plugin should be
-			-- installed and loaded.
 			cond = function()
 				return vim.fn.executable("make") == 1
 			end,
@@ -33,24 +27,24 @@ return {
 		pcall(require("telescope").load_extension, "ui-select")
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Find existing [B]uffers" })
+		vim.keymap.set("n", "<leader>sh", builtin.help_tags)
+		vim.keymap.set("n", "<leader>sk", builtin.keymaps)
+		vim.keymap.set("n", "<leader>sf", builtin.find_files)
+		vim.keymap.set("n", "<leader>ss", builtin.builtin)
+		vim.keymap.set("n", "<leader>sw", builtin.grep_string)
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep)
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
+		vim.keymap.set("n", "<leader>sr", builtin.resume)
+		vim.keymap.set("n", "<leader>s.", builtin.oldfiles)
+		vim.keymap.set("n", "<leader>sb", builtin.buffers)
 		vim.keymap.set("n", "<leader>/", function()
 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 				winblend = 10,
 				previewer = false,
 			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
+		end)
 		vim.keymap.set("n", "<leader>sn", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
+		end)
 	end,
 }
