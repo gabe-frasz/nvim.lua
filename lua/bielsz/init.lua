c1cf3d0c6ff2723c6586f90c3a4229d6696577fb
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2024-06-16 21:03:53 -0300

    add supermaven plugin

--- lua/bielsz/init.lua
+++ lua/bielsz/init.lua
@@ -2,6 +2,8 @@ require("bielsz.set")
 require("bielsz.remap")
 require("bielsz.lazy_init")
 
+require("supermaven-nvim.api").start()
+
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
