bab08649b748158cfe543267e6f2a14006d2adb1
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-04-27 14:38:47 -0300

    change nvim theme

--- lua/bielsz/lazy/colors.lua
+++ lua/bielsz/lazy/colors.lua
@@ -17,7 +17,36 @@ return {
 					italic = false,
 				},
 			})
-			ColorMyPencils()
+		end,
+	},
+	{
+		"zenbones-theme/zenbones.nvim",
+		-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+		-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+		-- In Vim, compat mode is turned on as Lush only works in Neovim.
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.background = "dark"
+			vim.g.zenbones_solid_nr = true
+			vim.g.zenbones_darken_comments = 45
+		end,
+		init = function()
+			ColorMyPencils("zenbones")
+		end,
+	},
+	{
+		"olivercederborg/poimandres.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("poimandres").setup({
+				disable_background = true,
+				styles = {
+					italic = false,
+				},
+			})
 		end,
 	},
 }
