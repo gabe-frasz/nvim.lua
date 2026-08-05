590e3014affe11ee34a2b60c61f9039551d0aa12
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2026-08-04 23:18:25 -0300

    feat(telescope): show hidden dotfiles in find_files picker

--- lua/bielsz/lazy/navigation.lua
+++ lua/bielsz/lazy/navigation.lua
@@ -37,6 +37,11 @@ return {
 		},
 		config = function()
 			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
