a800957421591694b4b1685595095727700c2035
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2026-08-04 22:34:22 -0300

    feat(utils): add overseer.nvim plugin setup

--- lua/bielsz/lazy/utils.lua
+++ lua/bielsz/lazy/utils.lua
@@ -1,20 +1,33 @@
 return {
-  {
-    "mbbill/undotree",
-    keys = {
-      { "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
-    },
-  },
-  {
-    "eandrju/cellular-automaton.nvim",
-    keys = {
-      { "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it rain" },
-    },
-  },
-  {
-    "theprimeagen/vim-be-good",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
+	{
+		"mbbill/undotree",
+		keys = {
+			{ "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
+		},
+	},
+	{
+		"eandrju/cellular-automaton.nvim",
+		keys = {
+			{ "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it rain" },
+		},
+	},
+	{
+		"theprimeagen/vim-be-good",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"stevearc/overseer.nvim",
+		---@module 'overseer'
+		---@type overseer.SetupOpts
+		opts = {
+			strategy = "toggleterm",
+		},
+		keys = {
+			{ "<leader>or", "<cmd>OverseerRun<CR>", desc = "Overseer Run" },
+			{ "<leader>ot", "<cmd>OverseerToggle<CR>", desc = "Overseer Toggle" },
+			{ "<leader>oa", "<cmd>OverseerTaskAction<CR>", desc = "Overseer Task Action" },
+		},
+	},
 }
